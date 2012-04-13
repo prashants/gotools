6de306bd7c0d8edcf41d6ca04c6936fb46908a81
Prashant P. Shah <pshah.mumbai@gmail.com>
2012-04-13 18:24:22 +0530

    ported portscan to go routines
    Signed-off-by: Prashant P. Shah <pshah.mumbai@gmail.com>

--- portscan.go
+++ portscan.go
@@ -10,7 +10,17 @@ import (
 	"os"
 )
 
+var ic = make(chan int, 1000)
 
+func port_scan(host string, port int) {
+	connstr := fmt.Sprintf("%s:%d", host, port)
+	conn, err := net.Dial("tcp", connstr)
+	if err == nil {
+		fmt.Printf("TCP Port open : %d\n", port)
+		conn.Close()
+	}
+	ic <- 1
+}
 
 func main() {
 	args := os.Args
@@ -29,12 +39,20 @@ func main() {
 	host := addr.String()
 
 	fmt.Println("Port scanning started for " + host + ".")
-	for port := 1; port <= 65535; port += 1 {
-		connstr := fmt.Sprintf("%s:%d", host, port)
-		conn, err := net.Dial("tcp", connstr)
-		if err == nil {
-			fmt.Printf("TCP Port open : %d\n", port)
-			conn.Close()
+
+	port := 1
+	counter := 1
+	for port <= 65535 {
+		/* running go routines max 1000 at a time */
+		for counter = 1; counter < 1000; counter += 1 { // 65535 // max 1018
+			go port_scan(host, port)
+			port += 1
+			if port > 65535 {
+				break
+			}
+		}
+		for chan_counter := 1; chan_counter < counter; chan_counter += 1 {
+			<-ic
 		}
 	}
 	fmt.Println("Port scanning finished.")
